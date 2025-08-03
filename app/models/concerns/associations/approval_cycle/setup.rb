0c512011e27e5f67b96c097284ae3a8c4582eecc
Farah Assaf <farah.asaf@zenhr.com>
2025-08-03 13:49:29 +0300

    fix approval cycle setup configuration initialization

--- app/models/concerns/associations/approval_cycle/setup.rb
+++ app/models/concerns/associations/approval_cycle/setup.rb
@@ -3,10 +3,13 @@ module Associations::ApprovalCycle::Setup
 
   included do
     belongs_to :level, polymorphic: true
-    has_many   :versions, -> { where(latest: false) }, foreign_key: "latest_setup_version_id", class_name: "ApprovalCycle::Setup"
-    has_many   :approval_cycle_approvers, -> { order(order: :asc) }, dependent: :destroy, class_name: "ApprovalCycle::Approver", foreign_key: "approval_cycle_setup_id", inverse_of: :approval_cycle_setup
-    has_many   :approval_cycle_action_takers, dependent: :destroy, class_name: "ApprovalCycle::ActionTaker", foreign_key: "approval_cycle_setup_id", inverse_of: :approval_cycle_setup
-    has_many   :approval_cycle_watchers,      dependent: :destroy, class_name: "ApprovalCycle::Watcher", foreign_key: "approval_cycle_setup_id", inverse_of: :approval_cycle_setup
-    ApprovalCycle.configuration.approval_cycle_setup_types.each_key { |type| has_many type.to_s.pluralize.to_sym, class_name: type.to_s.classify, foreign_key: "approval_cycle_setup_id" }
+    has_many   :versions, -> { where(latest: false) }, foreign_key: 'latest_setup_version_id', class_name: 'ApprovalCycle::Setup'
+    has_many   :approval_cycle_approvers, -> { order(order: :asc) }, dependent: :destroy, class_name: 'ApprovalCycle::Approver', foreign_key: 'approval_cycle_setup_id', inverse_of: :approval_cycle_setup
+    has_many   :approval_cycle_action_takers, dependent: :destroy, class_name: 'ApprovalCycle::ActionTaker', foreign_key: 'approval_cycle_setup_id', inverse_of: :approval_cycle_setup
+    has_many   :approval_cycle_watchers,      dependent: :destroy, class_name: 'ApprovalCycle::Watcher', foreign_key: 'approval_cycle_setup_id', inverse_of: :approval_cycle_setup
+
+    # Define dynamic associations safely, only if configuration is available
+    setup_types = ApprovalCycle.configuration&.approval_cycle_setup_types || {}
+    setup_types.each_key { |type| has_many type.to_s.pluralize.to_sym, class_name: type.to_s.classify, foreign_key: 'approval_cycle_setup_id' }
   end
 end
